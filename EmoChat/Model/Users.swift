4c00f88b11a271f87cf6f3471ffe4f591ad99d9c
k_tereschenko <ktereschenko@ksf.kiev.ua>
2017-05-30 14:46:00 +0300

    Revert "added some classes"
    This reverts commit 531ab425147a45584b1ebbdead8b1251729459cb.

--- EmoChat/Model/Users.swift
+++ EmoChat/Model/Users.swift
@@ -7,15 +7,9 @@
 //
 
 class Users {
-    var userId:String = "00000000-0000-0000-0000-000000000000"
-    var name: String? = ""
-    var email:String? = ""
-    
-    init(userId: String, name: String, email: String) {
-        self.userId = userId
-        self.name = name
-        self.email = email
-    }
+    let userId:String = "00000000-0000-0000-0000-000000000000"
+    let name: String? = ""
+    let email:String? = ""
 }
 
 
