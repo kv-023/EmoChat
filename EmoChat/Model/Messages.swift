a35e33ed6a666b358be5f870f6973db796432512
k_tereschenko <ktereschenko@ksf.kiev.ua>
2017-05-30 12:55:15 +0300

    create 3 classes of the model

---
+++ EmoChat/Model/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  EmoChat
+//
+//  Created by Kirill on 5/30/17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+class Mssages {
+    let messageId: String = "00000000-0000-0000-0000-000000000000"
+    let sender: String? = ""
+    let conversation: String? = ""
+}
+
---
+++ EmoChat/Model/Users.swift
@@ -0,0 +1,15 @@
+//
+//  Users.swift
+//  EmoChat
+//
+//  Created by Kirill on 5/30/17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+class Users {
+    let userId:String = "00000000-0000-0000-0000-000000000000"
+    let name: String? = ""
+    let email:String? = ""
+}
+
+
