fe110d6c29b71144359eb24a8979b5a818b4dc0d
Demetrius Zelensky <zellensky@gmail.com>
2017-05-30 16:22:42 +0300

    made test comment

--- EmoChat/ViewController.swift
+++ EmoChat/ViewController.swift
@@ -41,6 +41,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //
     }
     
     
