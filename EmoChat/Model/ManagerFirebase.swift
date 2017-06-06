22a613a5510f30b5f8d91dcfc4cd66a2777fb105
Olga Saliy <olia.white.1996@gmail.com>
2017-06-06 15:15:24 +0300

    Add class ManagerFirebase

---
+++ EmoChat/Model/ManagerFirebase.swift
@@ -0,0 +1,16 @@
+//
+//  ManagerFirebase.swift
+//  EmoChat
+//
+//  Created by Olga Saliy on 06.06.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class ManagerClass {
+    
+    var ref: FIRDatabaseReference?
+    
+}
