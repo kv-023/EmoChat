3803d9b800b5cbdd494cb1895ce840948ec0dd15
Andrew Tkachuk <andrewtkachuk@yahoo.com>
2017-06-06 14:01:24 +0300

    Create Sign Up view controller

---
+++ EmoChat/Controller/Helper/RegExpControl.swift
@@ -0,0 +1,9 @@
+//
+//  RegExpControll.swift
+//  EmoChat
+//
+//  Created by Andrii Tkachuk on 6/6/17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import Foundation
---
+++ EmoChat/Controller/SignUpControllers/SignUpFirstViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpFirstViewController.swift
+//  EmoChat
+//
+//  Created by Andrii Tkachuk on 6/6/17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class SignUpFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
