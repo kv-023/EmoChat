d249405a0fd738332044e68df32db56c523bf0fa
Demetrius Zelensky <zellensky@gmail.com>
2017-06-06 16:11:38 +0300

    test commit

--- EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
+++ EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
@@ -13,6 +13,8 @@ class SignUpChooseYourPhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
         // Do any additional setup after loading the view.
     }
 
