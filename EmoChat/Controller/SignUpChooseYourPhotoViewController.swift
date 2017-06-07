f25b778e3d7f3b059185bf0cd58308f3cc8d7fa3
Demetrius Zelensky <zellensky@gmail.com>
2017-06-07 13:47:03 +0300

    add user image on button

--- EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
+++ EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
@@ -11,8 +11,14 @@ import Firebase
 
 class SignUpChooseYourPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate   {
     
+    
+    @IBOutlet weak var image: UIButton!
+    
     @IBAction func chooseYourPhotoButton(_ sender: Any) {
-        print("button is pressed")
+        
+        
+
+        
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
@@ -34,34 +40,24 @@ class SignUpChooseYourPhotoViewController: UIViewController, UIImagePickerContro
         // Dispose of any resources that can be recreated.
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
         print(chosenImage.description)
         
+        image.setImage(chosenImage, for: .normal)
         
-        //            imageView.image = chosenImage
-        
-        
-        
+        //add image to firebase
         
         let storage = Storage.storage()
         let storageRef = storage.reference()
+        
+        //create reference
         let tempimageRef = storageRef.child("images/tmpImage.jpg")
         let metaData = StorageMetadata()
         metaData.contentType = "image/jpeg"
         
+        //add image to base
         tempimageRef.putData(UIImageJPEGRepresentation(chosenImage, 0.8)!, metadata: metaData) { (data, error) in
             if error == nil {
                 print("upload succesfull")
@@ -71,4 +67,13 @@ class SignUpChooseYourPhotoViewController: UIViewController, UIImagePickerContro
         }
         dismiss(animated:true, completion: nil)
     }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
