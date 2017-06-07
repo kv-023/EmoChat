6822a44c03f7bc545057623da299ecee3010c7e6
Demetrius Zelensky <zellensky@gmail.com>
2017-06-07 12:25:08 +0300

    add upload from gallery

--- EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
+++ EmoChat/Controller/SignUpChooseYourPhotoViewController.swift
@@ -7,37 +7,68 @@
 //
 
 import UIKit
+import Firebase
 
-class SignUpChooseYourPhotoViewController: UIViewController {
-    
+class SignUpChooseYourPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate   {
     
     @IBAction func chooseYourPhotoButton(_ sender: Any) {
-        print("test")
+        print("button is pressed")
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        print(chosenImage.description)
+        
+        
+        //            imageView.image = chosenImage
+        
+        
+        
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let tempimageRef = storageRef.child("images/tmpImage.jpg")
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        tempimageRef.putData(UIImageJPEGRepresentation(chosenImage, 0.8)!, metadata: metaData) { (data, error) in
+            if error == nil {
+                print("upload succesfull")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        }
+        dismiss(animated:true, completion: nil)
     }
-    */
-
 }
