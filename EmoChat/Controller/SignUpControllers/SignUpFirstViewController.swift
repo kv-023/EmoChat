1f1fdfda65e3af0ccd3ac324ae526245cfb0f608
Andrew Tkachuk <andrewtkachuk@yahoo.com>
2017-06-13 14:03:27 +0300

    Change ManagerFirabase methods usage in Sign Up first screen controller

--- EmoChat/Controller/SignUpControllers/SignUpFirstViewController.swift
+++ EmoChat/Controller/SignUpControllers/SignUpFirstViewController.swift
@@ -137,31 +137,41 @@ class SignUpFirstViewController: UIViewController, UITextFieldDelegate, RegexChe
             passwordLabel.printError(errorText: NSLocalizedString("Enter password", comment: "Empty password"))
             password.redBorder()
         }
-//        if success
-//            && (usernameValid
-//                && emailValid
-//                && passwordValid
-//                && passwordConfirmationValid) {
-//            manager?.signUp(email: email.text!, password: password.text!) {
-//                resultString in
-//                if resultString != "Success" {
-//                    self.emailLabel.printError(errorText: resultString)
-//                    self.email.redBorder()
-//                    unique = false
-//                }
-//            }
-//            manager?.checkUserNameUniqness(username.text!) {
-//                usernameResult in
-//                if !usernameResult {
-//                    self.username.redBorder()
-//                    self.emailLabel.printError(errorText: "Username already exists")
-//                    unique = false
-//                }
-//            }
-//            if unique {
-//                performSegue(withIdentifier: "additional", sender: self)
-//            }
-//        }
+        if success
+            && (usernameValid
+                && emailValid
+                && passwordValid
+                && passwordConfirmationValid) {
+            manager?.signUp(email: email.text!, password: password.text!) {
+                result in
+                switch result {
+                case .success:
+                    break
+                case .failure(let error):
+                    self.emailLabel.printError(errorText: error)
+                    self.email.redBorder()
+                    unique = false
+                default:
+                    break
+                }
+            }
+            manager?.checkUserNameUniqness(username.text!) {
+                result in
+                switch result {
+                case .success:
+                    break
+                case .failure(let error):
+                    self.username.redBorder()
+                    self.emailLabel.printError(errorText: error)
+                    unique = false
+                default:
+                    break
+                }
+            }
+            if unique {
+                performSegue(withIdentifier: "additional", sender: self)
+            }
+        }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
