f55513b4c8f6fb966e53a4117dbb3ec53bc7d174
vladyslavts <vladyslav@email.com>
2017-06-16 15:45:58 +0300

    Logo Changes

--- EmoChat/Controller/ChatSettingsTableViewController.swift
+++ EmoChat/Controller/ChatSettingsTableViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ChatSettingsTableViewController: UITableViewController {
 
+    //var users: Int = 0 = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,6 +25,9 @@ class ChatSettingsTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -28,25 +36,74 @@ class ChatSettingsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 3 
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+       
+        switch section {
+            
+            case 0: return 1
+            
+            case 1: return 2
+            
+            default: return 7
+            
+        }
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        var cell = UITableViewCell()
+        var cell2 = LogoTableViewCell()
+        
+        if indexPath.section == 0 {
+            
+            cell2 = tableView.dequeueReusableCell(withIdentifier: "logoCell", for: indexPath) as! LogoTableViewCell
+            cell2.conversLogo.image = UIImage.init(named: "1.png")
+            
+        } else if indexPath.section == 1 && indexPath.row == 0 {
+            
+                cell = tableView.dequeueReusableCell(withIdentifier: "addUser", for: indexPath)
+            
+        } else if indexPath.section == 1 && indexPath.row == 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "leaveChat", for: indexPath)
+            
+        } else if indexPath.section == 2 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "users", for: indexPath)
+        }
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 100
+        case 1:
+            return 43
+            
+        default:
+            return 60
+        }
+        
+    }
 
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Chat Settings"
+        } else if section == 2 {
+                return "111 users in conversation"
+            }
+         return ""
+        }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -92,4 +149,10 @@ class ChatSettingsTableViewController: UITableViewController {
     }
     */
 
+    
+  
+
 }
+
+
+
---
+++ EmoChat/Controller/LogoTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LogoTableViewCell.swift
+//  EmoChat
+//
+//  Created by Vladyslav Tsykhmystro on 16.06.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class LogoTableViewCell: UITableViewCell {
+
+    @IBOutlet var conversLogo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
