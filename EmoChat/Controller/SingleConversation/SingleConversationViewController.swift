ecc8a22664f02f28a42895628a1e7a05681f7610
Olga Saliy <olia.white.1996@gmail.com>
2017-06-20 12:31:35 +0300

    Add SingleConversationViewController

---
+++ EmoChat/Controller/SingleConversation/SingleConversationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SingleConversationViewController.swift
+//  EmoChat
+//
+//  Created by Olga Saliy on 20.06.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class SingleConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
