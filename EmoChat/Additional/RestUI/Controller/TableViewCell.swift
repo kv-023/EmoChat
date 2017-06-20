eb4ac1200411894a68e2a4186d696c9a510cb67a
Sergii Kyrychenko <4_ipx@mail.ru>
2017-06-20 12:51:49 +0300

    add classes to test rest API

---
+++ EmoChat/Additional/RestUI/Controller/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  EmoChat
+//
+//  Created by Sergii Kyrychenko on 19.06.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var myImageInCell: UIImageView!
+   
+    @IBOutlet weak var myLabel: UILabel!
+ 
+}
---
+++ EmoChat/Additional/RestUI/Controller/TableViewController.swift
@@ -0,0 +1,112 @@
+//
+//  TableViewController.swift
+//  EmoChat
+//
+//  Created by Sergii Kyrychenko on 19.06.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var stringURL: String?
+    var imageData: Data?
+    
+    
+
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    func getImage(info: String) {
+        stringURL = info
+        let task = URLSession.shared.dataTask(with: (URL(string: info))!) {(data, responce, error) in
+            
+            if error != nil {
+                print("error")
+            }
+            
+            else {
+                    self.imageData = data
+            }
+        }
+        task.resume()
+       
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath) as! TableViewCell
+        
+        if imageData != nil && stringURL != nil {
+            
+    cell.myImageInCell.image = UIImage(data: imageData!)
+    cell.myLabel.text = stringURL
+        
+        }
+    
+        
+      return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
