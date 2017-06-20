332b1b958472cd2d6f203499cba46ac06efdeaf8
Sergii Kyrychenko <4_ipx@mail.ru>
2017-06-20 13:36:15 +0300

    add JSON parser class and fun

--- EmoChat/Additional/RestUI/Controller/TableViewController.swift
+++ EmoChat/Additional/RestUI/Controller/TableViewController.swift
@@ -60,6 +60,7 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super .viewDidLoad()
+        GetJson(jsonString: "https://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback")
         
     }
     
