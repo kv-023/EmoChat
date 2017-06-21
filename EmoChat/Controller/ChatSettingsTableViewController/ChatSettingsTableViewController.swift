08bf3c3f5d949f25bc5fb38e0bc50dbab1ca48eb
Vladyslav <vladyslav@email.com>
2017-06-21 18:55:28 +0300

    Add userDefaults for saving conversation logo

--- EmoChat/Controller/ChatSettingsTableViewController/ChatSettingsTableViewController.swift
+++ EmoChat/Controller/ChatSettingsTableViewController/ChatSettingsTableViewController.swift
@@ -22,16 +22,15 @@ class ChatSettingsTableViewController: UITableViewController, UIImagePickerContr
     var manager: ManagerFirebase!
     var conversations: [Conversation]?
     
+    let userDefaults = UserDefaults.standard
+    let kDefaultsCellLogo = "conversationLogo"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         manager = ManagerFirebase.shared
         
-        let  iii = UIImage.init(named: "background.png")
-        let s = iii?.size.height
-        print(s)
-
         manager.getUserPicFullResolution(from: "userPics/ihn570HNGkY600i6ZUICS6YrqkQ2/519124722475.jpg") {
             result in
             switch result {
@@ -91,10 +90,7 @@ class ChatSettingsTableViewController: UITableViewController, UIImagePickerContr
             logoCell.conversLogo.contentMode = .scaleAspectFill
          //   logoCell.conversLogo.image = UIImage.init(named: "1.png")
             
-            let arrray: [UIImage] = [UIImage.init(named: "111.jpg")!, UIImage.init(named: "111.jpg")!/*, UIImage.init(named: "111.jpg")!, UIImage.init(named: "111.jpg")!, UIImage.init(named: "5.png")!, UIImage.init(named: "6.png")!*/]
-        
-            
-            
+            let arrray: [UIImage] = [UIImage.init(named: "1.png")!, UIImage.init(named: "1.png")!/*, UIImage.init(named: "111.jpg")!, UIImage.init(named: "111.jpg")!, UIImage.init(named: "5.png")!, UIImage.init(named: "6.png")!*/]
             
             //let convers = Conversation(conversationId: <#T##String#>, usersInConversation: <#T##[User]#>, messagesInConversation: <#T##[Message]?#>, lastMessage: <#T##Message?#>)
             var urlsArray = ["userPics/ihn570HNGkY600i6ZUICS6YrqkQ2/519124722475.jpg",
@@ -120,79 +116,78 @@ class ChatSettingsTableViewController: UITableViewController, UIImagePickerContr
         //                })
         //            }
         
-        logoCell.conversLogo.image = UIImage.createFinalImg(logoImages: arrray)
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(loadNewLogo))
-        logoCell.conversLogo.isUserInteractionEnabled = true
-        logoCell.conversLogo.addGestureRecognizer(tapGestureRecognizer)
+            if let imageData = userDefaults.value(forKey: kDefaultsCellLogo),
+                let image = UIImage(data: imageData  as! Data) {
+                
+                logoCell.conversLogo.image = image
+                
+            } else {
+            
+                logoCell.conversLogo.image = UIImage.createFinalImg(logoImages: arrray)
+            
+            }
+            
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(loadNewLogo))
+            logoCell.conversLogo.isUserInteractionEnabled = true
+            logoCell.conversLogo.addGestureRecognizer(tapGestureRecognizer)
         
         return logoCell
         
         case 1:
-        if indexPath.row == 0 {
-            addUserCell = tableView.dequeueReusableCell(withIdentifier: "invite", for: indexPath)
-            return addUserCell
-        } else {
-            leaveChatCell = tableView.dequeueReusableCell(withIdentifier: "leave", for: indexPath)
-            return leaveChatCell
-        }
-        
+            if indexPath.row == 0 {
+                addUserCell = tableView.dequeueReusableCell(withIdentifier: "invite", for: indexPath)
+                return addUserCell
+            } else {
+                leaveChatCell = tableView.dequeueReusableCell(withIdentifier: "leave", for: indexPath)
+                return leaveChatCell
+            }
+            
         default:
-        userCell = tableView.dequeueReusableCell(withIdentifier: "user", for: indexPath)
-        userCell.imageView?.image = UIImage.init(named: "male.png")
-        userCell.textLabel?.text = "Name LastName"
-        return userCell
+            userCell = tableView.dequeueReusableCell(withIdentifier: "user", for: indexPath)
+            userCell.imageView?.image = UIImage.init(named: "male.png")
+            userCell.textLabel?.text = "Name LastName"
+            return userCell
+        }
     }
-}
 
-//MARK: - UITableViewDelegate
+    //MARK: - UITableViewDelegate
 
-/*
- override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
- switch section {
- case 2:
- return "Users"
- default:
- return ""
- }
- }
-*/
-
-
-override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
     
-    if section == 1 {
-        return 20
-    } else {
-        return 0
+        if section == 1 {
+            return 20
+        } else {
+            return 0
+        }
+    
     }
-    
-}
 
-override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     
-    switch indexPath.section {
-    case 0:
-        return 75
-    case 1:
-        return 40
-    default:
-        return 50
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+        switch indexPath.section {
+        case 0:
+            return 75
+        case 1:
+            return 40
+        default:
+            return 50
+        }
+    
     }
-    
-}
 
-override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
     
-    if indexPath.section == 0 {
-        return nil
-    } else {
-        return indexPath
+        if indexPath.section == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
     }
-}
 
-override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-}
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 
 /*
@@ -242,60 +237,46 @@ override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: Inde
 
 
 // MARK: Loading New Photo
-func loadNewLogo(tapGestureRecognizer: UITapGestureRecognizer) {
+    func loadNewLogo(tapGestureRecognizer: UITapGestureRecognizer) {
     
-    let imagePicker = UIImagePickerController()
+        let imagePicker = UIImagePickerController()
     
-    imagePicker.delegate = self
-    imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
     
-    let alert = UIAlertController(title: "Image Source", message: "Choose new conversation logo", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "Image Source", message: "Choose new conversation logo", preferredStyle: .actionSheet)
     
-    alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {
-        action in
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {
+            action in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                
             imagePicker.sourceType = .photoLibrary
             self.present(imagePicker, animated: true, completion: nil)
+            }
         }
-    }
+            )
         )
-    )
     
-    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
     
-    self.present(alert, animated: true, completion: nil)
-    self.present(imagePicker, animated: true, completion: nil)
+        self.present(alert, animated: true, completion: nil)
+        self.present(imagePicker, animated: true, completion: nil)
+    }
     
-    
-}
 
-func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     
-    guard let chosenImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
-    UIGraphicsBeginImageContext(CGSize(width: 100, height: 100))
+        guard let chosenImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
     
-   chosenImage.draw(in: CGRect(x: 0, y: 0, width: 100, height: 100))
-    let imgg = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    
-    
-    logoCell.conversLogo.clipsToBounds = true
-    logoCell.conversLogo.image = imgg
-    
-    
-    var imageHeight = imgg?.size.height
-    var imageWidth = imgg?.size.width
-    
-    print ("QQQQQQQQQQQQ ======= \(imageHeight)")
-    //print (imageWidth)
-    
-    print(imgg as Any)
-    
-       self.dismiss(animated:true, completion: nil)
-}
+        logoCell.conversLogo.clipsToBounds = true
+        logoCell.conversLogo.image = chosenImage
+        
+        let imgData = UIImageJPEGRepresentation(chosenImage, 1)
+        
+        userDefaults.set(imgData, forKey: kDefaultsCellLogo)
+        self.dismiss(animated:true, completion: nil)
+    }
 
-
-//MARK: - Mix Photos
 
 
 }
