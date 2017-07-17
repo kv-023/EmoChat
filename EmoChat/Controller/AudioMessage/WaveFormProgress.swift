4ec445f1f2c3d8aa40c9c5e1ebe093872008626b
Sergii Kyrychenko <4_ipx@mail.ru>
2017-07-17 14:31:43 +0300

    Get current state Develop branch

---
+++ EmoChat/Controller/AudioMessage/WaveFormProgress.swift
@@ -0,0 +1,29 @@
+//
+//  WaveFormProgress.swift
+//  EmoChat
+//
+//  Created by Sergii Kyrychenko on 16.07.17.
+//  Copyright © 2017 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+
+struct waveFormProgressPath {
+    var upperPath: [UIBezierPath] = []
+    var lowerPath: [UIBezierPath] = []
+}
+
+class WaveFormProgress: UIView {
+
+    
+    
+    
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+}
