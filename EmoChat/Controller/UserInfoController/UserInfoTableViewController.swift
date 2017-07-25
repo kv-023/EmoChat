bb432720559b662f70330f00361d3873df552083
Vladyslav <vladyslav@email.com>
2017-07-25 14:21:59 +0300

    Add segue userinfoVC. Display name and number. Add localize string

--- EmoChat/Controller/UserInfoController/UserInfoTableViewController.swift
+++ EmoChat/Controller/UserInfoController/UserInfoTableViewController.swift
@@ -16,8 +16,30 @@ class UserInfoTableViewController: UITableViewController {
     @IBOutlet weak var phoneNumber: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     
+    var selectedUser: User!
+    var selectedUserPhoto: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userPic.image = selectedUserPhoto
+        
+        if let name = selectedUser.firstName, let second = selectedUser.secondName {
+            nameLabel.text = name + " " + second
+        } else {
+            nameLabel.text = selectedUser.username
+        }
+        
+        if let phone = selectedUser.phoneNumber {
+            phoneNumber.text = phone
+        } else {
+            phoneNumber.text = NSLocalizedString("Unknown", comment: "")
+            phoneNumber.textColor = .gray
+        }
+        if let name = selectedUser.firstName {
+            navigationItem.title = name
+        }
+        
+        usernameLabel.text = "@" + selectedUser.username
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -30,72 +52,4 @@ class UserInfoTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-        /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }*/
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
